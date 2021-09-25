913aeb8b68fdc4278862da7fb3f4f34637e81626
Dmitriy Ovsyannikov <dnovsyannikov@sberbank.ru>
2021-09-25 15:02:28 +0500

    create regexp component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
+<template>
+  <reg-exp-controller :config="data" />
+</template>
+
 <script>
+import RegExpController from '@/components/RegExpController.vue';
+import data from '@/fake/data';
+
 export default {
   name: 'App',
+  components: {
+    RegExpController,
+  },
+  setup() {
+    return {
+      data,
+    };
+  },
 };
 </script>
---
+++ src/components/RegExpController.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="reg-exp-controller" />
+</template>
+
+<script>
+export default {
+  name: 'RegExpController',
+  props: {
+    config: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style scoped>
+.reg-exp-controller {
+  width: 100%;
+  height: 100%;
+}
+</style>
